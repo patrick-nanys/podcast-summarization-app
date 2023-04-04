952816f78b38817ceeb8a3f3a97637a6b3a7c0a9
splint3rsec <khalil.lem@protonmail.com>
2023-04-05 00:47:01 +0200

    Added templating and static rendering

--- src/main.py
+++ src/main.py
@@ -1,12 +1,18 @@
 # FastAPI main file
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+staticfiles = StaticFiles(directory="static")
+app.mount("/static", staticfiles, name="static")
 
-@app.get("/")
-def index():
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
     """
     Index route.
     """
-    return {"alive":"True"}
+    return templates.TemplateResponse("index.html", {"request": request, "title": "BrevioCast"})
