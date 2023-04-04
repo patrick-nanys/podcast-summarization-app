3021273f969d4d0ad0e11e8ffcfd982ef315db7a
splint3rsec <khalil.lem@protonmail.com>
2023-04-05 01:20:26 +0200

    Created signup & login routes

--- src/main.py
+++ src/main.py
@@ -16,3 +16,17 @@ def index(request: Request):
     Index route.
     """
     return templates.TemplateResponse("index.html", {"request": request, "title": "BrevioCast"})
+
+@app.get("/signup", response_class=HTMLResponse)
+def signup(request: Request):
+    """
+    Signup route. (GET)
+    """
+    return templates.TemplateResponse("register.html", {"request": request})
+
+@app.get("/login", response_class=HTMLResponse)
+def login(request: Request):
+    """
+    Login route. (GET)
+    """
+    return templates.TemplateResponse("login.html", {"request": request})
