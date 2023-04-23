f50dd3f664669061372ceca5e1ca64e41efb72d0
splint3rsec <khalil.lem@protonmail.com>
2023-04-23 17:40:05 +0200

    Added podcasts model

--- src/db/models.py
+++ src/db/models.py
@@ -1,8 +1,8 @@
 """ DB Models file for SQLAlchemy """
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer, ForeignKey
 from .uuid_gen import GUID
 import uuid
-# from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship
 
 from .database import Base
 
@@ -16,4 +16,18 @@ class User(Base):
     username = Column(String, unique=True, nullable=False, index=True)
     email = Column(String, unique=True, nullable=False, index=True)
     hashed_password = Column(String, nullable=False)
-    # podcasts = relationship("Podcast", back_populates="owner") # To check later
+
+    podcasts = relationship("Podcast", back_populates="owner")
+
+class Podcast(Base):
+    """
+    Podcast model
+    """
+    __tablename__ = "podcasts"
+
+    id = Column(Integer, primary_key=True, nullable=False, index=True)
+    filename = Column(String, nullable=False, index=True)
+    owner_id = Column(GUID(), ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="podcasts")
+
--- src/db/schemas.py
+++ src/db/schemas.py
@@ -2,15 +2,30 @@
 from uuid import UUID
 from pydantic import BaseModel, Field, EmailStr
 
+# Users schemas
 class UserBase(BaseModel):
     email: EmailStr
     username: str = Field(min_length=4)
 
 class UserCreate(UserBase):
     password: str
-    
+
 class User(UserBase):
     id: UUID
 
     class Config:
         orm_mode = True # needed for podcasts relationship
+
+# Podcasts schemas
+class PodcastBase(BaseModel):
+    filename: str
+
+class PodcastCreate(PodcastBase):
+    pass
+
+class Podcast(PodcastBase):
+    id: int
+    owner_id: UUID
+
+    class Config():
+        orm_mode = True
