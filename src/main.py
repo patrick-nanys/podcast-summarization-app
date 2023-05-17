548ed7589264ac6c6eb4893e15476fd6e1beb5c5
splint3rsec <khalil.lem@protonmail.com>
2023-05-17 17:56:25 +0200

    Restructure backend

--- src/main.py
+++ src/main.py
@@ -1,129 +1,107 @@
-""" FastAPI main file """
-from fastapi import FastAPI, Request, Depends, HTTPException
-from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi import FastAPI, Request, Response
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
-from sqlalchemy.orm import Session
-from uuid import UUID
-from utils.middleware import AWS
-from authx import Authentication, EncodeDBBackend, HTTPCache, cache
-from pathlib import Path
-import logging
-import re
-
-import db.database as db
-import cache.redis_conf as rd
-from db import schemas
+from fastapi.templating import Jinja2Templates
+from authx import Authentication
+from authx.services.auth import AuthService
+from authx.backend import UsersRepo
+from authx.core.jwt import JWTBackend
+from authx.routers.auth import get_router
 
 
 app = FastAPI()
-auth = Authentication(backend=EncodeDBBackend(database=db.database, users=db.users, email_confirmation=db.email_confirmation))
-HTTPCache.init(redis_url=rd.REDIS_URL, namespace='breviocast_cache')
-templates = Jinja2Templates(directory="templates")
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-app.include_router(db.auth.auth_router, prefix="/api/users")
-app.include_router(db.auth.password_router, prefix="/api/users")
-app.include_router(db.auth.admin_router, prefix="/api/users")
-app.include_router(db.auth.search_router, prefix="/api/users")
-
-# This will be deleted later, just for testing purposes.
-# s3_handler = AWS("eu-west-1", config["AWS"]["aws_access_key_id"], config["AWS"]["aws_secret_access_key"])
-# file_name = Path("static/mp3/music.mp3")
-# s3_handler.upload_to_bucket(file_name, "breviocast-prod")
-
-
-@app.get("/", response_class=HTMLResponse)
-@cache(key="c.home", ttl_in_seconds=180)
-async def index(request: Request):
-    """
-    Index route.
-    """
-    return templates.TemplateResponse("index.html", {"request": request, "title": "BrevioCast"})
-
-@app.get("/signup/", response_class=HTMLResponse)
-def signup(request: Request):
-    """
-    Signup route. (GET)
-    """
-    return templates.TemplateResponse("register.html", {"request": request})
-
-
-@app.post("/signup/", response_model=schemas.User)
-def signup(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    """
-    Signup route. (POST)
-
-    TODO:
-        + Add password confirmation check
-        + Handle same email used twice with different username, unique constraint on sqlite
-    """
-    db_user = crud.get_user_by_username(db, username=user.username)
-    if db_user:
-        raise HTTPException(status_code=400, detail="User already exists")
-    return crud.create_user(db=db, user=user)
+auth = Authentication(
+    debug=True,
+    base_url="http://localhost:8000/",
+    site="http://localhost:8000/",
+    database_backend="breviocast",
+    callbacks="http://localhost:8000/callback",
+    access_cookie_name="access_token",
+    refresh_cookie_name="refresh_token",
+    private_key="private.pem",
+    public_key="public.pem",
+    access_expiration=3600,
+    refresh_expiration=86400,
+    smtp_username=None,
+    smtp_host=None,
+    smtp_password=None,
+    smtp_tls=False,
+    display_name="breviocast",
+    recaptcha_secret=None,
+    social_creds=None,
+    social_providers=None
+)
+AuthService.setup(
+        repo = UsersRepo,
+        auth_backend = JWTBackend,
+        debug = True,
+        base_url = 'http://localhost:8000',
+        site = 'http://localhost:8000/',
+        recaptcha_secret = None,
+        smtp_username = None,
+        smtp_password = None,
+        smtp_host = None,
+        smtp_tls = False,
+        display_name = 'authx',
+    )
+templates = Jinja2Templates(directory="frontend")
+app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
+app.include_router(auth.auth_router, prefix="/api/users")
 
 
-@app.get("/login/", response_class=HTMLResponse)
-def login(request: Request):
-    """
-    Login route. (GET)
-    """
-    return templates.TemplateResponse("login.html", {"request": request})
+@app.get('/', response_class=HTMLResponse)
+async def homepage(request: Request):
+    """Index"""
+    return templates.TemplateResponse("home.html", {"request": request})
 
-
-@app.get("/users/", response_model=list[schemas.User])
-def get_users(start_from: int = 0, until: int = 10, db: Session = Depends(get_db)):
-    """
-    Display all users information. (GET)
-
-    TODO:
-        + Add authorization check
-    """
-    users_list = crud.get_users(db=db, start_from=start_from, until=until)
-    return users_list
-
-
-@app.get("/user_info/{user_id}", response_model=schemas.User)
-def get_user_info(user_id: UUID, db: Session = Depends(get_db)):
-    """
-    Display user information. (GET)
-
-    TODO:
-        + Add authorization check
-        + Handle UUID not found -> we get 500 response instead of 404
-    """
-    user = crud.get_user_by_id(db=db, user_id=user_id)
-    if user is None:
-        return HTTPException(status_code=404, detail="User not found")
-    return user
-
-
-@app.get("/request/", response_class=HTMLResponse)
-@app.post("/request/")
-async def request_podcast(request: Request):
-    """
-    Request Podcast route. (GET/POST)
-    """
+@app.get('/register.html', response_class=HTMLResponse, name="auth:register")
+@app.post('/register', response_class=HTMLResponse, name="auth:register")
+async def signup(request: Request, response: Response):
+    """Sign up"""
     if request.method == "GET":
-        return templates.TemplateResponse("request.html", {"request": request})
-    elif request.method == "POST":
-        requested_links = []
-        ytb_regex_pattern = (
-            r"^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:watch\?.*v=|v\/)|youtu\.be\/)([\w\-]{11})(?:$|[^\w\-])"
-        )
-        try:
-            form_data = await request.form()
-            url = form_data["url"]
-            match = re.search(ytb_regex_pattern, url)
-            if match:
-                requested_links.append(url)  # TODO: insert in database instead of list pushing.
-                print(requested_links)
-                return {"message": f"your link {url} was submitted successfully!"}
-            else:
-                redirect_url = request.url_for("request_podcast")
-                return RedirectResponse(redirect_url, status_code=303)
-        except Exception as e:
-            logging.exception(f"caught exception: {e}")
-            return HTTPException(status_code=500, detail="Server timeout, please try again.")
+        return templates.TemplateResponse("register.html", {"request": request})
+    # elif request.method == "POST":
+    #     try:
+    #         #TODO: Create a user and add it to the DB
+    #         data = await request.json()
+    #         service = AuthService()
+    #         tokens = await service.register(data)
+    #         AuthService.set_tokens_in_response(response, tokens)
+    #     except Exception as e:
+    #         print(f"Some exception occured, details: {e}")
+    #     return None
+
+
+@app.get('/signin.html', response_class=HTMLResponse)
+@app.post('/login', response_class=HTMLResponse)
+async def signin(request: Request):
+    """Sign in"""
+    if request.method == "GET":
+        return templates.TemplateResponse("signin.html", {"request": request})
+    # elif request.method == "POST":
+    #     try:
+    #         #TODO: Create a user and add it to the DB
+    #         data = await request.json()
+    #         service = AuthService()
+    #         tokens = await service.register(data)
+    #         AuthService.set_tokens_in_response(response, tokens)
+    #     except Exception as e:
+    #         print(f"Some exception occured, details: {e}")
+    #     return None
+
+@app.post('/logout', name="auth:logout")
+async def logout(*, response: Response):
+    """Logout"""
+    response.delete_cookie(auth.access_cookie_name)
+    response.delete_cookie(auth.refresh_cookie_name)
+    return None
+
+@app.get('/browse', response_class=HTMLResponse)
+async def browse(request: Request):
+    """Browse"""
+    return templates.TemplateResponse("app.html", {"request": request})
+
+@app.get('/podcast/{id}', response_class=HTMLResponse)
+async def podcast(request: Request):
+    """Podcast"""
+    return templates.TemplateResponse("explore.html", {"request": request})
