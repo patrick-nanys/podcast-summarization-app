27d678ad76ac1a38ad67b16873612f2d1c7ccb30
splint3rsec <khalil.lem@protonmail.com>
2023-05-20 00:27:12 +0200

    Add configuration handling

--- src/main.py
+++ src/main.py
@@ -3,10 +3,16 @@ from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
+from utils import AWS, conf_helper
 
 app = FastAPI()
 templates = Jinja2Templates(directory=Path("../frontend"))
 app.mount("/static", StaticFiles(directory=Path("../frontend/static")), name="static")
+config = conf_helper.read_configuration()
+
+s3_handler = AWS(config["AWS"]["region"], config["AWS"]["aws_access_key_id"], config["AWS"]["aws_secret_access_key"])
+file_name = Path("static/mp3/music.mp3")
+s3_handler.upload_to_bucket(file_name, "breviocast-prod")
 
 
 @app.get('/', response_class=HTMLResponse)
