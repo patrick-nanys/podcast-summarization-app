8c362747039b1b028b7e2246edddfb986a21ce2e
splint3rsec <khalil.lem@protonmail.com>
2023-05-21 13:49:54 +0200

    get podcast metadata from s3

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
+import csv
 import json
 from fastapi import FastAPI, Request, Form, HTTPException
-from fastapi.responses import HTMLResponse, RedirectResponse, JSONResponse
+from fastapi.responses import HTMLResponse, RedirectResponse, JSONResponse, StreamingResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
@@ -69,11 +70,43 @@ async def browse(request: Request):
 async def podcast(name: str, request: Request):
     """Podcast"""
     try:
-        file_metadata = s3_handler.fetch_podcast_from_bucket(bucket=config["AWS"]["bucket"], name=name)
-        content = json.loads(file_metadata["Body"].read())
+        # TODO: separate this name search in an external file as a function
+        if name == "naval_how_to_get_rich":
+            podcast_name = "How to Get Rich"
+            # timestamps json
+            podcast_chunks_start_timestamps = s3_handler.fetch_podcast_from_bucket(bucket=config["AWS"]["bucket"], name=name+"/"+podcast_name+"_chunk_start_timestamps.json")
+            podcast_chunks_start_timestamps_result = json.loads(podcast_chunks_start_timestamps["Body"].read())
+            # json
+            podcast_chunks = s3_handler.fetch_podcast_from_bucket(bucket=config["AWS"]["bucket"], name=name+"/"+podcast_name+"_chunks.json")
+            json_content_chunks = json.loads(podcast_chunks["Body"].read())
+            # mp3
+            podcast_mp3_summary = s3_handler.fetch_podcast_from_bucket(bucket=config["AWS"]["bucket"], name=name+"/"+podcast_name+"_read_summary.mp3")
+            def iter_mp3_content():
+                """Internal function to iterate s3 object content, for mp3 files"""
+                for chunk in podcast_mp3_summary["Body"].iter_chunks():
+                    yield chunk
+            podcast_mp3_summary_result = StreamingResponse(iter_mp3_content(), media_type="audio/mpeg")
+            # txt
+            podcast_summary_txt = s3_handler.fetch_podcast_from_bucket(bucket=config["AWS"]["bucket"], name=name+"/"+podcast_name+"_summarized_text.json")
+            podcast_summary_txt_result = json.loads(podcast_summary_txt["Body"].read())
+            # csv
+            podcast_transcription_csv = s3_handler.fetch_podcast_from_bucket(bucket=config["AWS"]["bucket"], name="naval_how_to_get_rich"+"/"+"How to Get Rich"+"_transcription.csv")
+            def iter_csv_content():
+                for chunk in podcast_transcription_csv["Body"].iter_chunks():
+                    yield chunk
+            podcast_transcription_csv_result = StreamingResponse(iter_csv_content(), media_type="text/csv")
+
     except Exception as e:
         logging.exception(f"caught exception: {e}")
         return HTTPException(
             status_code=500, detail="Server timeout, please try again."
         )
-    return JSONResponse(content=content) # TODO: pass it as a context variable to the FE
+    return templates.TemplateResponse("app.html",
+                                        {
+                                            "request": request,
+                                            "podcast_chunks_start_timestamps_result":podcast_chunks_start_timestamps_result,
+                                            "json_content_chunks":json_content_chunks,
+                                            "podcast_mp3_summary_result":podcast_mp3_summary_result,
+                                            "podcast_summary_txt_result":podcast_summary_txt_result,
+                                            "podcast_transcription_csv_result":podcast_transcription_csv_result
+                                        })
