526cbb96ed4e1e6548f57676c0c270da730bc231
splint3rsec <khalil.lem@protonmail.com>
2023-05-21 15:51:04 +0200

    Add create podcast entry operation

---
+++ src/db/operation.py
@@ -0,0 +1,12 @@
+"""Database operation file for Podcast storing"""
+
+from sqlalchemy.orm import Session
+from . import model, schema
+
+def push_podcast_to_db(db: Session, podcast: schema.AddPodcast):
+    """Add requested link to the database"""
+    db_podcast = model.Podcast(id=podcast.id, link=podcast.link)
+    db.add(db_podcast)
+    db.commit()
+    db.refresh(db_podcast)
+
