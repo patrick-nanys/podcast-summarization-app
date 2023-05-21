6d3f9a9d9e42fd288167aa70b7dcff53747f9c53
splint3rsec <khalil.lem@protonmail.com>
2023-05-21 14:41:11 +0200

    Refactor get_podcast_data_by_name

--- src/utils/podcast.py
+++ src/utils/podcast.py
@@ -17,52 +17,30 @@ def get_podcast_data_by_name(name: str):
     else:
         if name == "naval_how_to_get_rich":
             podcast_name = "How to Get Rich"
-            # timestamps json
-            podcast_chunks_start_timestamps = s3_handler.fetch_podcast_from_bucket(bucket=config["AWS"]["bucket"], name=name+"/"+podcast_name+"_chunk_start_timestamps.json")
-            podcast_chunks_start_timestamps_result = json.loads(podcast_chunks_start_timestamps["Body"].read())
-            # json
-            podcast_chunks = s3_handler.fetch_podcast_from_bucket(bucket=config["AWS"]["bucket"], name=name+"/"+podcast_name+"_chunks.json")
-            json_content_chunks = json.loads(podcast_chunks["Body"].read())
-            # mp3
-            podcast_mp3_summary = s3_handler.fetch_podcast_from_bucket(bucket=config["AWS"]["bucket"], name=name+"/"+podcast_name+"_read_summary.mp3")
-            def iter_mp3_content():
-                """Internal function to iterate s3 object content, for mp3 files"""
-                for chunk in podcast_mp3_summary["Body"].iter_chunks():
-                    yield chunk
-            podcast_mp3_summary_result = StreamingResponse(iter_mp3_content(), media_type="audio/mpeg")
-            # txt
-            podcast_summary_txt = s3_handler.fetch_podcast_from_bucket(bucket=config["AWS"]["bucket"], name=name+"/"+podcast_name+"_summarized_text.json")
-            podcast_summary_txt_result = json.loads(podcast_summary_txt["Body"].read())
-            # csv
-            podcast_transcription_csv = s3_handler.fetch_podcast_from_bucket(bucket=config["AWS"]["bucket"], name=name+"/"+podcast_name+"_transcription.csv")
-            def iter_csv_content():
-                for chunk in podcast_transcription_csv["Body"].iter_chunks():
-                    yield chunk
-            podcast_transcription_csv_result = StreamingResponse(iter_csv_content(), media_type="text/csv")
         elif name == "uranium_mining_outputs":
             podcast_name = "How Uranium Mining Works  STUFF YOU SHOULD KNOW"
-            # timestamps json
-            podcast_chunks_start_timestamps = s3_handler.fetch_podcast_from_bucket(bucket=config["AWS"]["bucket"], name=name+"/"+podcast_name+"_chunk_start_timestamps.json")
-            podcast_chunks_start_timestamps_result = json.loads(podcast_chunks_start_timestamps["Body"].read())
-            # json
-            podcast_chunks = s3_handler.fetch_podcast_from_bucket(bucket=config["AWS"]["bucket"], name=name+"/"+podcast_name+"_chunks.json")
-            json_content_chunks = json.loads(podcast_chunks["Body"].read())
-            # mp3
-            podcast_mp3_summary = s3_handler.fetch_podcast_from_bucket(bucket=config["AWS"]["bucket"], name=name+"/"+podcast_name+"_read_summary.mp3")
-            def iter_mp3_content():
-                """Internal function to iterate s3 object content, for mp3 files"""
-                for chunk in podcast_mp3_summary["Body"].iter_chunks():
-                    yield chunk
-            podcast_mp3_summary_result = StreamingResponse(iter_mp3_content(), media_type="audio/mpeg")
-            # txt
-            podcast_summary_txt = s3_handler.fetch_podcast_from_bucket(bucket=config["AWS"]["bucket"], name=name+"/"+podcast_name+"_summarized_text.json")
-            podcast_summary_txt_result = json.loads(podcast_summary_txt["Body"].read())
-            # csv
-            podcast_transcription_csv = s3_handler.fetch_podcast_from_bucket(bucket=config["AWS"]["bucket"], name=name+"/"+podcast_name+"_transcription.csv")
-            def iter_csv_content():
-                for chunk in podcast_transcription_csv["Body"].iter_chunks():
-                    yield chunk
-            podcast_transcription_csv_result = StreamingResponse(iter_csv_content(), media_type="text/csv")
+        # timestamps json
+        podcast_chunks_start_timestamps = s3_handler.fetch_podcast_from_bucket(bucket=config["AWS"]["bucket"], name=name+"/"+podcast_name+"_chunk_start_timestamps.json")
+        podcast_chunks_start_timestamps_result = json.loads(podcast_chunks_start_timestamps["Body"].read())
+        # json
+        podcast_chunks = s3_handler.fetch_podcast_from_bucket(bucket=config["AWS"]["bucket"], name=name+"/"+podcast_name+"_chunks.json")
+        json_content_chunks = json.loads(podcast_chunks["Body"].read())
+        # mp3
+        podcast_mp3_summary = s3_handler.fetch_podcast_from_bucket(bucket=config["AWS"]["bucket"], name=name+"/"+podcast_name+"_read_summary.mp3")
+        def iter_mp3_content():
+            """Internal function to iterate s3 object content, for mp3 files"""
+            for chunk in podcast_mp3_summary["Body"].iter_chunks():
+                yield chunk
+        podcast_mp3_summary_result = StreamingResponse(iter_mp3_content(), media_type="audio/mpeg")
+        # txt
+        podcast_summary_txt = s3_handler.fetch_podcast_from_bucket(bucket=config["AWS"]["bucket"], name=name+"/"+podcast_name+"_summarized_text.json")
+        podcast_summary_txt_result = json.loads(podcast_summary_txt["Body"].read())
+        # csv
+        podcast_transcription_csv = s3_handler.fetch_podcast_from_bucket(bucket=config["AWS"]["bucket"], name=name+"/"+podcast_name+"_transcription.csv")
+        def iter_csv_content():
+            for chunk in podcast_transcription_csv["Body"].iter_chunks():
+                yield chunk
+        podcast_transcription_csv_result = StreamingResponse(iter_csv_content(), media_type="text/csv")
     return {
             "Timestamps": podcast_chunks_start_timestamps_result,
             "Chunks": json_content_chunks,
